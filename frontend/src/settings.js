2d96161527643a51926737f220dd74eef645c26f
Adam Yedidia <adamyedidia@gmail.com>
2024-02-09 17:24:34 -0500

    Fix

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,1 +1,1 @@
-export const URL = process.env.REACT_APP_URL || 'http://ec2-34-192-101-140.compute-1.amazonaws.com:5007';
+export const URL = process.env.REACT_APP_URL || 'http://5.161.227.62:5023';
