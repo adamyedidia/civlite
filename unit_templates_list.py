d8dbc9c01e7925e0bf5be51bd8d50514faf2454d
dfarhi <32961271+dfarhi@users.noreply.github.com>
2024-02-27 14:21:17 -0800

    remove scouts

--- unit_templates_list.py
+++ unit_templates_list.py
@@ -1,16 +1,4 @@
 UNITS = {
-    "Scout": {
-        "name": "Scout",
-        "building_name": None,
-        "type": "military",
-        "metal_cost": 5,
-        "wood_cost": 0,
-        "strength": 2,
-        "movement": 2,
-        "range": 1,
-        "tags": [],
-        "abilities": [],
-    },
     "Warrior": {
         "name": "Warrior",
         "building_name": None,
