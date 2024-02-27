b453eee6bb8b9e7a5b4a065408c5d0cc25cdf9ac
Adam Yedidia <adamyedidia@gmail.com>
2024-02-26 21:15:17 -0500

    Defensive units must be in the city center

--- city.py
+++ city.py
@@ -415,12 +415,13 @@ class City:
         best_hex = None
         best_hex_distance_from_target = 10000
 
-        for hex in self.hex.get_neighbors(game_state.hexes):
-            if not hex.is_occupied(unit.type, self.civ):
-                distance_from_target = hex.distance_to(self.get_closest_target() or self.hex)
-                if distance_from_target < best_hex_distance_from_target:
-                    best_hex = hex
-                    best_hex_distance_from_target = distance_from_target
+        if "defensive" not in unit.tags:
+            for hex in self.hex.get_neighbors(game_state.hexes):
+                if not hex.is_occupied(unit.type, self.civ):
+                    distance_from_target = hex.distance_to(self.get_closest_target() or self.hex)
+                    if distance_from_target < best_hex_distance_from_target:
+                        best_hex = hex
+                        best_hex_distance_from_target = distance_from_target
 
         best_unit_to_reinforce = None
         if best_hex is None:
@@ -435,7 +436,7 @@ class City:
             #             best_hex_distance_from_target = distance_from_target            
 
             for hex in [self.hex, *self.hex.get_neighbors(game_state.hexes)]:
-                if hex.is_occupied(unit.type, self.civ):
+                if hex.is_occupied(unit.type, self.civ) and ("defensive" not in unit.tags or hex.coords == self.hex.coords):
                     unit_to_possibly_reinforce = hex.units[0]
                     if unit_to_possibly_reinforce.civ.id == self.civ.id and unit_to_possibly_reinforce.template.name == unit.name and unit_to_possibly_reinforce.hex:
                         unit_penalty = unit_to_possibly_reinforce.health * 10 + unit_to_possibly_reinforce.hex.distance_to(self.get_closest_target() or self.hex)
