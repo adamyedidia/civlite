a4dd5b99432f8d77ebd8536ddd2ee2e483f5560a
dfarhi <32961271+dfarhi@users.noreply.github.com>
2024-03-04 12:14:56 -0800

    escape cancels decline view

--- frontend/src/GamePage.js
+++ frontend/src/GamePage.js
@@ -405,6 +405,7 @@ export default function GamePage() {
                 setFoundingCity(false);
                 setShowFlagArrows(false);
                 setTechChoices(null);
+                setDeclineOptionsView(false);
             }
         };
     
