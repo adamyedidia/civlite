ff0616efceb3316f0017c4a6e7662a2dae3ed408
dfarhi <32961271+dfarhi@users.noreply.github.com>
2024-03-13 20:37:50 -0700

    task bar (#52)

---
+++ frontend/src/TaskBar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './TaskBar.css';
+
+import { WithTooltip } from './WithTooltip';
+
+import cityImg from './images/city.png';
+import scienceImg from './images/science.png';
+import tradeHubImg from './images/tradehub.png';
+
+// For images in the public/images directory, use the public URL path
+const flag1Img = `${process.env.PUBLIC_URL}/images/flag.svg`;
+const flag2Img = `${process.env.PUBLIC_URL}/images/purple_flag.svg`;
+
+const TaskIcon = ({icon, onClick, tooltip}) => {
+    return <div className={`task-icon ${onClick ? 'clickable' : ''}`} onClick={onClick}>
+        <WithTooltip tooltip={tooltip}>
+            <img src={icon} />
+        </WithTooltip>
+    </div>
+}
+
+
+export const TaskBar = ({myCiv, myCities, canFoundCity, setSelectedCity, setFoundingCity, setTechChoiceDialogOpen}) => {
+    const anyUnhappyCities = myCities?.some(city => city.unhappiness > 0 || city.projected_income['unhappiness'] > 0);
+    
+    // Generate the list of unhappy cities as a JSX element
+    const unhappyCitiesList = (
+        <ul>
+            {myCities?.filter(city => city.unhappiness > 0 || city.projected_income['unhappiness'] > 0)
+                .map(city => (
+                    <li key={city.name} onClick={() => setSelectedCity(city)}>{city.name}</li> // Ensure each <li> has a unique key
+                ))}
+        </ul>
+    );
+
+    return <div className="task-bar">
+        {canFoundCity && <TaskIcon icon={cityImg} onClick={() => {setFoundingCity(true)}} tooltip="Found city" />}
+        {!myCiv?.researching_tech_name && <TaskIcon icon={scienceImg} onClick={() => {setTechChoiceDialogOpen(true)}} tooltip="Choose research" />}
+        {!myCiv?.trade_hub_id && anyUnhappyCities && <TaskIcon icon={tradeHubImg} 
+            tooltip={<div>Select trade hub (in city window). Unhappy cities: {unhappyCitiesList}</div>}
+        />}
+        {!myCiv?.target1 && <TaskIcon icon={flag1Img} tooltip="Select primary flag" />}
+        {!myCiv?.target2 && <TaskIcon icon={flag2Img} tooltip="Select secondary flag" />}
+    </div>
+}
+
+export default TaskBar;
+
