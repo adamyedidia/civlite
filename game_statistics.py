7291daa8b0f6aad7f819ed33703e487003853134
Adam Yedidia <adamyedidia@gmail.com>
2024-05-08 23:59:01 -0400

    More stats

--- game_statistics.py
+++ game_statistics.py
@@ -27,6 +27,10 @@ def make_game_statistics_plots(sess, game_id: str):
 
     turn_nums = []
     decline_turns = defaultdict(list)
+    decline_turns_for_civs = defaultdict(list)    
+
+    total_yields_by_turn = defaultdict(list)
+    military_strength_by_turn = defaultdict(list)
 
     for frame in animation_frames:
         game_state = GameState.from_json(frame.game_state)
@@ -50,6 +54,30 @@ def make_game_statistics_plots(sess, game_id: str):
             game_player = game_state.game_player_by_player_num[player_num]
             if old_civ_ids_by_player and old_civ_ids_by_player[player_num] != game_player.civ_id:
                 decline_turns[game_player.username].append(frame.turn_num)
+                decline_turns_for_civs[game_player.civ_id].append(frame.turn_num)
+
+        yields_by_civ = defaultdict(float)
+        total_metal_value_by_civ = defaultdict(int)
+
+        civ_scores_by_turn = defaultdict(list)
+        cum_civ_scores_by_turn = defaultdict(list)
+
+        for civ_id, civ in game_state.civs_by_id.items():
+            cum_civ_scores_by_turn[civ_id].append(civ.score)
+            civ_scores_by_turn[civ_id].append(civ.score - (cum_civ_scores_by_turn[civ_id][-2] if len(cum_civ_scores_by_turn[civ_id]) > 1 else 0))
+        
+        for city_id in game_state.cities_by_id:
+            city = game_state.cities_by_id[city_id]
+            city.adjust_projected_yields(game_state)
+            total_yields = city.projected_income['food'] + city.projected_income['wood'] + city.projected_income['metal'] + city.projected_income['science']
+            yields_by_civ[city.civ_id] += total_yields
+
+        for unit in game_state.units:
+            total_metal_value_by_civ[unit.civ_id] += unit.template.metal_cost
+
+        for civ_id, civ in game_state.civs_by_id.items():
+            total_yields_by_turn[civ_id].append(yields_by_civ[civ_id])
+            military_strength_by_turn[civ_id].append(total_metal_value_by_civ[civ_id])
 
 
     print('Plotting: ', scores_by_turn)
@@ -100,3 +128,23 @@ def make_game_statistics_plots(sess, game_id: str):
         for decline_turn in decline_turns[username]:
             plt.axvline(decline_turn, color=line_color, linestyle=dash_style)  # Use the same color but different dash style for the vertical line
         plt.savefig(f"plots/cum_scores_by_player_{game_id}.png")
+
+
+    for civ_id, yields in total_yields_by_turn.items():
+        line, = plt.plot(turn_nums, yields, label=civ_id)
+        plt.legend()
+        line_color = line.get_color()
+        dash_style = next(dash_styles)
+        for decline_turn in decline_turns_for_civs[civ_id]:
+            plt.axvline(decline_turn, color=line_color, linestyle=dash_style)
+        plt.savefig(f"plots/total_yields_by_civ_{game_id}.png")
+
+
+    for civ_id, military_strength in military_strength_by_turn.items():
+        line, = plt.plot(turn_nums, military_strength, label=civ_id)
+        plt.legend()
+        line_color = line.get_color()
+        dash_style = next(dash_styles)
+        for decline_turn in decline_turns_for_civs[civ_id]:
+            plt.axvline(decline_turn, color=line_color, linestyle=dash_style)
+        plt.savefig(f"plots/military_strength_by_civ_{game_id}.png")
