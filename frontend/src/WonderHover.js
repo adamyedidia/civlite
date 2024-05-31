99d7dab952c7c2b11b12cce9d589a75dc3ce88f2
dfarhi <32961271+dfarhi@users.noreply.github.com>
2024-05-30 20:41:14 -0700

    oops

--- frontend/src/WonderHover.js
+++ frontend/src/WonderHover.js
@@ -8,7 +8,7 @@ const WonderHover = ({ wonder, templates}) => {
                 <h2>{wonder.name}</h2>
                 <p>{wonder.vp_reward} vp</p>
                 <p>{wonder.description}</p>
-                {<UnitDisplay unit={templates.UNITS[wonder.hover_unit_name]} />}
+                {wonder.hover_unit_name && <UnitDisplay unit={templates.UNITS[wonder.hover_unit_name]} />}
             </div>
 };
 
