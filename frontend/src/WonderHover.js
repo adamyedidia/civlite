d997c59248bca673b59cd610a1762e921bab7dc0
dfarhi <32961271+dfarhi@users.noreply.github.com>
2024-11-07 09:36:26 -0800

    show wonder wood cost

--- frontend/src/WonderHover.js
+++ frontend/src/WonderHover.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './WonderHover.css';
 import UnitDisplay from './UnitDisplay';
 import { romanNumeral } from './TechListDialog';
+import woodImg from './images/wood.png';
 
 
 const WonderHover = ({ wonder, templates}) => {
@@ -9,6 +10,7 @@ const WonderHover = ({ wonder, templates}) => {
                 <h2>{romanNumeral(wonder.advancement_level)}. {wonder.name}</h2>
                 <div className="wonder-card-subtitle">
                 <div>Age {romanNumeral(wonder.advancement_level)} Wonder</div>
+                <div>{wonder.cost} <img src={woodImg} alt="wood" height="15px"/></div>
                 </div>
                 {wonder.description.map((desc, i) => <p key={i}>{desc}</p>)}
                 {wonder.hover_unit_name && <UnitDisplay template={templates.UNITS[wonder.hover_unit_name]} />}
